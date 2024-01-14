b23c6365c30552ee53f6eeacdfc133fef88ff668
henhadayo1 <henhadayo@gmail.com>
2024-01-14 16:45:56 +0200

    Use useApi hook and get menuData from server

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
-import { menuItemsData } from "../data/menu-data";
+import useApi from "../hooks/useApi";
+import { MenuItem } from "../models/MenuItem";
 import MenuWithStyle from "./MenuWithStyle";
 
 const Navbar: React.FC = () => {
+  const { data: menuItemsData, isLoading, error } = useApi<MenuItem[]>("http://localhost:3000/menu", { method: "GET" });
+
   return (
     <div>
-      Menu
-      <MenuWithStyle items={menuItemsData} />
+      {isLoading && <span>Fetching data...</span>}
+      {error && <span>{error}</span>}
+      {menuItemsData && (
+        <>
+          Menu
+          <MenuWithStyle items={menuItemsData} />
+        </>
+      )}
+      {/* Menu
+      <MenuWithStyle items={menuItemsData} /> */}
     </div>
   );
 };
